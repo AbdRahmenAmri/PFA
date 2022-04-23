7555974a0de422dc07b16ecb4dfc4c4f06b36fa5
HajAliMohamed <mohamedhajali45@gmail.com>
2022-04-23 23:13:23 +0100

    add_axios_requests

--- Frontend/src/frontend/src/Components/signin/SignIn.js
+++ Frontend/src/frontend/src/Components/signin/SignIn.js
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./signIn.css"
 import img from "./undraw_community_re_cyrm.svg"
+import axios from 'axios'
 function SignIn() {
+
+  const[registerForm,setRegisterForm] = useState({
+    fullName : "",
+    email :"",
+    password : "",
+    confirmePassword : ""
+  })
+
+  function handleChange(e){
+    const{name , value} = e.target
+    setRegisterForm(prevValue=>{
+        switch (name) {
+          case "fullName":
+
+              return{
+                fullName : value,
+                email : prevValue.email,
+                password : prevValue.password,
+                confirmePassword : prevValue.confirmePassword
+              }
+
+          case "email":
+            return{
+              fullName : prevValue.fullName,
+              email : value,
+              password : prevValue.password,
+              confirmePassword : prevValue.confirmePassword
+            }
+
+          case "password":
+            return{
+              fullName : prevValue.fullName,
+              email : prevValue.email,
+              password : value,
+              confirmePassword : prevValue.confirmePassword
+            } 
+            
+          case "confirmePassword":
+            return{
+              fullName : prevValue.fullName,
+              email : prevValue.email,
+              password : prevValue.password,
+              confirmePassword : value
+            }  
+        
+          default:
+            break;
+        }
+    })
+  }
+
+  function handleVerif(){
+    if((registerForm.fullName.length ===0)|| (registerForm.email.indexOf("@")===-1)||(registerForm.password.length<8)||(registerForm.confirmePassword.length<8)||(registerForm.password !== registerForm.confirmePassword)){
+      return false
+    }else{
+      return true
+    }
+
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const verif = handleVerif()
+   if(verif){
+    axios.post(`https://jsonplaceholder.typicode.com/users`,registerForm)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+   }else{
+    const smalls = Array.from(document.querySelectorAll(".main .container_signin .right_div form small")) 
+    smalls.forEach((s)=>{
+      s.style.display = "block"
+    })
+   }
+  }
+
   return (
     <div className='main'>
             <div className='container_signin'>
@@ -14,16 +92,17 @@ function SignIn() {
       </div>
       <div className='right_div'>
         <h2>Register</h2>
-        <form>
-          <input type="text" name="fullname" placeholder='FullName'/>
-          <input type="email" name="email" placeholder='Email'/>
-          <small>Invalid email @ !</small>
-          <input type="text" name="password" placeholder='Password'/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="fullName" placeholder='FullName' onBlur={handleVerif} onChange={handleChange}/>
+          <small>your name should be more than 0 characters</small>
+          <input type="email" name="email" placeholder='Email' onBlur={handleVerif}  onChange={handleChange}/>
+          <small>email should conatin "@" , "." !</small>
+          <input type="password" name="password" placeholder='Password' onBlur={handleVerif}  onChange={handleChange}/>
           <small>password should be more than 8 characters</small>
-          <input type="password" name="confirmePassword" placeholder='Confirme Password'/>
+          <input type="password" name="confirmePassword" placeholder='Confirme Password' onBlur={handleVerif}  onChange={handleChange}/>
           <small>confirme password should be identical</small>
-          <input className='registerButton' type="submit" value="Register" />
-          <Link to='/Login'>Have an account ? login</Link>
+          <input className='registerButton' type="submit" value="Register"/>
+          <Link to='/login'>Have an account ? login</Link>
         </form>
       </div>
     </div>
