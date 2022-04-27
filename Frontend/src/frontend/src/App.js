726645e6b93f6596127ac24c8cbd206ab05870aa
AbdRahmen Amri <abdrahmen.amri@fsb.ucar.tn>
2022-04-27 04:51:23 +0100

    merge landing page + login + singup + forgetpassword + pages

--- Frontend/src/frontend/src/App.js
+++ Frontend/src/frontend/src/App.js
@@ -1,35 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import './App.css';
-import LandingPage from './Components/landingPage/LandingPage';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
 
 import Login from './Components/login/Login'
 import {Notfound,Terms,Policy} from './Components/pages/*'
 
-
-
-function App() {
-    return (
-      <>
-      <Router>
-      <Routes>
-        <Route  path='/' element={<LandingPage/>}></Route>
-        <Route  path='/login' element={<Login/>}></Route>
-        <Route  path='/register' element={<Login/>}></Route>
-        <Route  path='/terms' element={<Terms/>}></Route>
-        <Route  path='/policy' element={<Policy/>}></Route>
-        <Route  path='/*' element={<Notfound/>}></Route>
-      </Routes>
-      </Router>
-      </>
-    );
-=======
 import {Routes , Route , BrowserRouter} from "react-router-dom"
-import './App.css';
 import ForgetPassword from "./Components/forgetPassword/ForgetPassword";
 import LandingPage from "./Components/landingPage/LandingPage"
 import Login from "./Components/login/Login";
@@ -45,12 +21,14 @@ function App() {
         <Route path="/Login" element={<Login/>} />
         <Route path="/Signin" element={<SignIn/>} />
         <Route path="/ForgetPassword" element={<ForgetPassword/>} />
+        <Route  path='/terms' element={<Terms/>}></Route>
+        <Route  path='/policy' element={<Policy/>}></Route>
+        <Route  path='/*' element={<Notfound/>}></Route>
       </Routes>
     </BrowserRouter>
 
     </div>
   );
->>>>>>> 4e0af05b970e65ee6f9c1958799e1716712a408c
 }
 
 export default App;
